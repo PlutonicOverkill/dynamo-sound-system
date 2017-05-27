0d10aab246232fe4c0118065eea77b8375410723
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2017-05-27 19:15:24 +1200

    Finish build script

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ rm -r -- bin/
 # create directories for the binaries
 mkdir bin
 mkdir bin/gdcc
-mkdir bin/ir
-mkdir bin/build
+mkdir bin/ir # binaries built with gdcc go here
+mkdir bin/build # this is what goes into the final .pk3
 
 echo Getting latest build of GDCC...
 echo
@@ -25,10 +25,35 @@ echo
 echo Downloading GDCC...
 echo
 
+# download gdcc
 curl -L $latest_gdcc > bin/gdcc.7z
 
 echo
 echo Unpacking GDCC...
 echo
 
+# extract gdcc
 7z x "bin/gdcc.7z" -obin/gdcc -aoa
+
+# get filename for built .pk3
+git_version=$(git describe --long --tags --dirty --always)
+build_name='dynamic_music_system-'$git_version'.pk3'
+
+# force recompilation of libc
+rm bin/build/libc.lib
+
+# run the compile script
+. ./compile.sh
+
+# which should be piped through dev/null, but my
+# version of bash doesn't support it for some reason
+path_to_executable=$(which appveyor)
+if [ -x "$path_to_executable" ] ; then
+    rm -r -- bin/appveyor/
+	mkdir bin/appveyor
+	cp bin/$build_name bin/appveyor/dynamic_music_system.pk3
+	./$path_to_executable PushArtifact bin/appveyor/dynamic_music_system.pk3
+fi
+
+echo
+echo Build completed.
