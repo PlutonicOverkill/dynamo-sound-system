9f14ec63b7dfe21d859f004ea32b11d9e24a601d
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2017-05-27 19:24:36 +1200

    Do some refactoring

--- build.sh
+++ build.sh
@@ -2,18 +2,11 @@ echo ======== Dynamic Music System for ZDoom ========
 echo
 echo Build started...
 echo
-
-# clear out the build folder
-rm -r -- bin/
-
-# create directories for the binaries
-mkdir bin
-mkdir bin/gdcc
-mkdir bin/ir # binaries built with gdcc go here
-mkdir bin/build # this is what goes into the final .pk3
-
 echo Getting latest build of GDCC...
 echo
+
+rm -- tools/
+mkdir tools
 
 # get dropbox URL to latest version of GDCC
 latest_gdcc=$(curl -L https://www.dropbox.com/sh/e4msp35vxp61ztg/AAA2Ti5pBthuMBfwPSb6Y_MCa/gdcc_master-latest_win64.txt?dl=1)
@@ -33,14 +26,16 @@ echo Unpacking GDCC...
 echo
 
 # extract gdcc
-7z x "bin/gdcc.7z" -obin/gdcc -aoa
+7z x "bin/gdcc.7z" -otools/gdcc -aoa
 
 # get filename for built .pk3
 git_version=$(git describe --long --tags --dirty --always)
 build_name='dynamic_music_system-'$git_version'.pk3'
 
 # force recompilation of libc
-rm bin/build/libc.lib
+# this is because the new version
+# of gdcc might be different
+rm bin/build/acs/libc.lib
 
 echo
 echo Compiling...
@@ -60,7 +55,7 @@ echo
 # version of bash doesn't support it for some reason
 path_to_executable=$(which appveyor)
 if [ -x "$path_to_executable" ] ; then
-    rm -r -- bin/appveyor/
+    rm -- bin/appveyor/
 	mkdir bin/appveyor
 	cp bin/$build_name bin/appveyor/dynamic_music_system.pk3
 	./$path_to_executable PushArtifact bin/appveyor/dynamic_music_system.pk3
---
+++ compile.sh
@@ -0,0 +1,8 @@
+# clear out the build folder
+rm -r -- bin/
+
+# create directories for the binaries
+mkdir bin
+mkdir bin/gdcc
+mkdir bin/ir # binaries built with gdcc go here
+mkdir bin/build # this is what goes into the final .pk3
