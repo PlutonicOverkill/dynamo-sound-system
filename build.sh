88795ad6880bff051a439c1bf78d4bf27a9b58d9
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2017-06-01 17:09:14 +1200

    Separate downloading GDCC into a seperate setup script

--- build.sh
+++ build.sh
@@ -4,38 +4,6 @@ echo ======== Dynamic Music System for ZDoom ========
 echo
 echo Build started...
 echo
-echo Getting latest build of GDCC...
-echo
-
-if [ -d tools ]; then
-    rm -r tools/
-fi
-
-mkdir tools
-
-if ! [ -d bin ]; then
-    mkdir bin
-fi
-
-# get dropbox URL to latest version of GDCC
-latest_gdcc=$(curl -L https://www.dropbox.com/sh/e4msp35vxp61ztg/AAA2Ti5pBthuMBfwPSb6Y_MCa/gdcc_master-latest_win64.txt?dl=1)
-
-# change downloaded URL to end in 'dl=1'
-latest_gdcc=${latest_gdcc/dl=0/dl=1}
-
-echo
-echo Downloading GDCC...
-echo
-
-# download gdcc
-curl -L $latest_gdcc > bin/gdcc.7z
-
-echo
-echo Unpacking GDCC...
-echo
-
-# extract gdcc
-7z x "bin/gdcc.7z" -otools/gdcc -aoa
 
 # get filename for built .pk3
 git_version=$(git describe --long --tags --dirty --always)
