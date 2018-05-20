1aa32d85c3fc7ce4da8c70e29cfdc111297b5db4
PlutonicOverkill <PlutonicOverkill@users.noreply.github.com>
2018-05-20 16:42:51 +1200

    Update the build scripts

---
+++ run.sh
@@ -0,0 +1,2 @@
+read -r gamepath < gamepath.txt
+"$gamepath" -iwad doom2 -file "bin/build" +map test_map &
--- setup.sh
+++ setup.sh
@@ -11,18 +11,12 @@ if ! [ -d bin ]; then
     mkdir bin
 fi
 
-# get dropbox URL to latest version of GDCC
-latest_gdcc=$(curl -L https://www.dropbox.com/sh/e4msp35vxp61ztg/AAA2Ti5pBthuMBfwPSb6Y_MCa/gdcc_master-latest_win64.txt?dl=1)
-
-# change downloaded URL to end in 'dl=1'
-latest_gdcc=${latest_gdcc/dl=0/dl=1}
-
 echo
 echo Downloading GDCC...
 echo
 
 # download gdcc
-curl -L $latest_gdcc > bin/gdcc.7z
+curl -L "https://www.dropbox.com/sh/e4msp35vxp61ztg/AACoOWfWHArqaNP4huJ0UzUpa/gdcc/gdcc-0.13.3-1-win64.7z?dl=1" > bin/gdcc.7z
 
 echo
 echo Unpacking GDCC...
